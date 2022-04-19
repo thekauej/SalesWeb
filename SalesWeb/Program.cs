8f474522381f1785ca1efdd343bbe3abae63426b
Kaue Jorge de Melo Machado <thekauek@gmail.com>
2022-04-19 16:14:42 -0300

    commit message

---
+++ SalesWeb/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWeb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    Console.WriteLine("teste");
+                    Console.WriteLine("teste2");
+                    Console.WriteLine("teste3");
+                    Console.WriteLine("Teste4");
+                    Console.WriteLine("Teste5");
+                });
+    }
+}
