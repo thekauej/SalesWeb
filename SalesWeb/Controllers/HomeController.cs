2f73d5a9714c30bed1cceb5a11b5d989c4c63258
Kaue Jorge de Melo Machado <thekauek@gmail.com>
2022-04-20 13:52:42 -0300

    First controller and Razer pages tests

--- SalesWeb/Controllers/HomeController.cs
+++ SalesWeb/Controllers/HomeController.cs
@@ -28,6 +28,13 @@ namespace SalesWeb.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Salles Web MVC App from C# Course";
+            ViewData["Aluno"] = "Kaue Jorge";
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
