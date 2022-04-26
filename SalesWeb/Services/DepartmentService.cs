78e13b1e89ce387e75357c087955b327b4d5a1f9
Kaue Jorge de Melo Machado <thekauek@gmail.com>
2022-04-25 21:01:21 -0300

    Asynchronous operation using Rasks (async, await)

--- SalesWeb/Services/DepartmentService.cs
+++ SalesWeb/Services/DepartmentService.cs
@@ -2,6 +2,8 @@
 using System.Linq;
 using System.Collections.Generic;
 using SalesWeb.Models;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWeb.Services
 {
@@ -14,10 +16,9 @@ namespace SalesWeb.Services
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
-
     }
 }
